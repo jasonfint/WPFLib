f010282ad06b42d13d44ad02d1b14b46c0a65114
jason.zhang <jason.zhang@fintgroup.com>
2019-05-15 16:11:43 +0800

    newprojectTest

---
+++ ExampleBrowser/Examples/DataTemplate/Element.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Element.cs" company="Helix Toolkit">
+//   Copyright (c) 2014 Helix Toolkit contributors
+// </copyright>
+// <summary>
+//   Represents an element.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataTemplateDemo
+{
+    using System.Windows.Media.Media3D;
+
+    /// <summary>
+    /// Represents an element.
+    /// </summary>
+    public class Element
+    {
+        /// <summary>
+        /// Gets or sets the material.
+        /// </summary>
+        public Material Material { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        public Point3D Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the radius.
+        /// </summary>
+        public double Radius { get; set; }
+    }
+}
